cee9ac25dc9dfc7e1b990d3cc531825bdfbdaee3
Vitrolo <viniciusrabelodominguesdasilva@gmail.com>
2025-07-23 10:54:14 -0300

    opa

--- exercicio01/script.js
+++ exercicio01/script.js
@@ -13,3 +13,23 @@ function sub(){
     // resposta.textContent = numero1 + numero2
     document.getElementById('respSub').textContent = numero1 - numero2
 }
+
+function multi(){
+    var numero1 = parseFloat(document.getElementById("n5").value)
+    var numero2 = parseFloat(document.getElementById("n6").value)
+    // var resposta = document.getElementById('resposta')
+    // resposta.textContent = numero1 + numero2
+    document.getElementById('respMulti').innerHTML = numero1 * numero2
+}
+
+function div(){
+    var numero1 = parseFloat(document.getElementById("n3").value)
+    var numero2 = parseFloat(document.getElementById("n4").value)
+    var resposta = document.getElementById('respDiv')
+    if(numero2 !== 0){
+        resposta.innerHTML = numero1 / numero2
+    }else{
+        resposta.textContent = "Não se divide por 0"
+    }
+ 
+}
